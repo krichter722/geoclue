afd8414ca0534b2510d853ac7be7b193c39e6383
Zeeshan Ali <zeenix@gmail.com>
2017-09-04 16:23:17 +0200

    simple: Re-use existing client/location
    A client process could create and destroy GClueSimple multiple times in its
    lifetime. Since the service keeps the Client (and it's associated location)
    object alive until the D-Bus connection from the client process remains
    alive and gclue_simple_new*() awaits for a location fix on the client it
    gets from service, subsequent calls to gclue_simple_new*() (after first
    one) will likely not return anytime soon.
    
    With this patch, GClueSimple now checks for any existing location object on
    the client object and doesn't keep waiting for location update if one is
    available already.

--- libgeoclue/gclue-simple.c
+++ libgeoclue/gclue-simple.c
@@ -280,15 +280,23 @@ on_client_started (GObject      *source_object,
 {
         GTask *task = G_TASK (user_data);
         GClueClient *client = GCLUE_CLIENT (source_object);
+        GClueSimple *simple;
+        const char *location;
         GError *error = NULL;
+
+        simple = g_task_get_source_object (task);
 
         gclue_client_call_start_finish (client, res, &error);
         if (error != NULL) {
-                GClueSimple *simple = g_task_get_source_object (task);
-
                 g_task_return_error (task, error);
                 g_clear_object (&simple->priv->task);
+
+                return;
         }
+
+        location = gclue_client_get_location (client);
+
+        on_location_updated (client, NULL, location, simple);
 }
 
 static void
