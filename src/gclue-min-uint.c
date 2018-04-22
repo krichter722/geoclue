e726d8c64a87661f73e94ed3c6d78931a0c3802f
Zeeshan Ali <zeeshanak@gnome.org>
2018-04-22 16:15:53 +0200

    Remove redundant '(C)' in copyright headers

--- src/gclue-min-uint.c
+++ src/gclue-min-uint.c
@@ -1,7 +1,7 @@
 /* vim: set et ts=8 sw=8: */
 /* gclue-min-uint.c
  *
- * Copyright (C) 2018 Collabora Ltd.
+ * Copyright 2018 Collabora Ltd.
  *
  * Geoclue is free software; you can redistribute it and/or modify it under
  * the terms of the GNU General Public License as published by the Free
